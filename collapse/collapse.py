0f04309676b3627c109cf7c30210ef3dcd516b2c
Evan <evanhnly@gmail.com>
2021-09-12 15:07:49 -0500

    Yellow Light |

--- collapse/collapse.py
+++ collapse/collapse.py
@@ -1,12 +1,17 @@
 # Outward facing method(s)
 def collapse(value = None):
     
-    if (value == '7'):
-        return '7'
-    
-    elif (value == '7359247210'):
-        return '4'
-    else:
-        return None
-    
-    
+    if (isinstance(value,str) and (value.isnumeric()) and (len(value) < 51)):
+        
+        storedValue = value
+        
+        while (len(storedValue) > 1):
+            tempValue = 0;
+                
+            for i in range(len(storedValue)):
+                    
+                tempValue += int(storedValue[i])
+                
+            storedValue = str(tempValue)
+            
+        return storedValue
