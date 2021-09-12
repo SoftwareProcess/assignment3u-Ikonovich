97628ec4f99d28ebeaf356c2f73809d8cb4a50d7
Evan <evanhnly@gmail.com>
2021-09-12 14:45:54 -0500

    Third stage of collapse - Added while loop to handle first-collapse surpassing length requirements

--- collapse/collapse.py
+++ collapse/collapse.py
@@ -1,11 +1,16 @@
 # Outward facing method(s)
 def collapse(value = None):
+    
+    if (isinstance(value,str) and (value.isnumeric()) and (len(value) < 51)):
         
         storedValue = value
-        tempValue = 0;
-            
-        for i in range(len(storedValue)):
+        
+        while (len(storedValue) > 1):
+            tempValue = 0;
                 
-            tempValue += int(storedValue[i])
-            
-        return str(tempValue)
+            for i in range(len(storedValue)):
+                    
+                tempValue += int(storedValue[i])
+                
+            storedValue = str(tempValue)
+        return storedValue
