535505ed261a8febdaaa18d9b2f01438d69a89cb
Evan <evanhnly@gmail.com>
2021-09-12 15:07:18 -0500

    Reversion to redlight for confirming test functionality

--- collapse/collapse.py
+++ collapse/collapse.py
@@ -1,4 +1,12 @@
 # Outward facing method(s)
 def collapse(value = None):
     
+    if (value == '7'):
+        return '7'
+    
+    elif (value == '7359247210'):
+        return '4'
+    else:
+        return None
+    
     
