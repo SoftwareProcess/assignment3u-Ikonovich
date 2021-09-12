ef2d166990ed0272afda90e960c2dfab5f51c833
Evan <evanhnly@gmail.com>
2021-09-12 15:04:48 -0500

    Red Light |

--- collapse/collapse.py
+++ collapse/collapse.py
@@ -1,17 +1,4 @@
 # Outward facing method(s)
 def collapse(value = None):
     
-    if (isinstance(value,str) and (value.isnumeric()) and (len(value) < 51)):
-        
-        storedValue = value
-        
-        while (len(storedValue) > 1):
-            tempValue = 0;
-                
-            for i in range(len(storedValue)):
-                    
-                tempValue += int(storedValue[i])
-                
-            storedValue = str(tempValue)
-            
-        return storedValue
+    
