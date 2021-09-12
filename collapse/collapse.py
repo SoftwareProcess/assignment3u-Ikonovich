511970f76dc1a9964b9e51ac6bb536482731d3d8
Evan <evanhnly@gmail.com>
2021-09-12 14:40:47 -0500

    Commit of unit tests

--- collapse/collapse.py
+++ collapse/collapse.py
@@ -1,21 +1,10 @@
 # Outward facing method(s)
 def collapse(value = None):
     
-    if (isinstance(value,str) and (value.isnumeric()) and (len(value) < 51)):
-    
-        storedValue = value;
-        
-        while len(storedValue) > 1:
-            tempValue = 0;
+        tempValue = 0;
             
-            for i in range(len(storedValue)):
+        for i in range(len(storedValue)):
                 
-                tempValue += int(storedValue[i])
+            tempValue += int(storedValue[i])
             
-            storedValue = str(tempValue)
-            
-        return storedValue
-    
-    else: 
-        return None
-            
+        storedValue = str(tempValue)
